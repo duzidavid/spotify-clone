a0f8d6326e6274a503b0ff4159ca1924d865bb1b
duzidavid <david.duzi@teliacompany.com>
2021-08-03 18:45:23 +0200

    added redirect to spotify

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import 'bootstrap/dist/bootstrap/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Login from './Login'
 
--- client/src/Login.js
+++ client/src/Login.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Button } from 'react-bootstrap'
 
 const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=4812520680ff45bf8e87a5ef6ab95e71&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state'
 
 
 export default function Login() {
     return (
-        <div>
-
-        </div>
+        <Container className="d-flex justify-content-center align-items-center" style={{ minHeight: "100vh" }}>
+            <Button variant="success" href={AUTH_URL}>Login with SPOTIFY</Button>
+        </Container>
     )
 }
